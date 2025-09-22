26f4cf47b927ea1fede8f30633969ad23962a4d0
Neptune <felix@ladislas.fr>
2025-09-22 19:54:52 +0200

    DB Setup

--- Server/docker/db/db.entrypoint.sql
+++ Server/docker/db/db.entrypoint.sql
@@ -1,15 +1,10 @@
-CREATE DATABASE Tarot;
-USE Tarot;
+CREATE DATABASE Pingu;
+USE Pingu;
 
 CREATE TABLE Users (
     ID int NOT NULL AUTO_INCREMENT,
-    FirstName varchar(255) NOT NULL,
-    LastName varchar(255) NOT NULL,
     Username varchar(50) NOT NULL,
-    Email varchar(255),
-    Birthdate DATE NOT NULL,
     Pwd varchar(255) NOT NULL,
-    Elo int,
     isAdmin int NOT NULL,
     PRIMARY KEY (ID)
 );
--- Server/www/includes/config.php
+++ Server/www/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbName = '';
+$dbName = 'Pingu';
 $addr = getenv('DB_HOST');
 $MySQLusername = getenv('DB_HOST');
 $MySQLpwd = getenv('DB_HOST');
