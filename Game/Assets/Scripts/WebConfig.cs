16241942abf819e402ac41e405a0ae77aa38fdbd
Neptune <felix@ladislas.fr>
2025-09-22 20:13:11 +0200

    Add .env webconfig for unity

---
+++ Game/Assets/Scripts/WebConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WebConfig", menuName = "Scriptable Objects/WebConfig")]
+public class WebConfig : ScriptableObject
+{
+    public string webServerAddress;
+}
